02e3d600279360c2c47e3906bad191d6c5811c40
katerinamerkulova <59052477+katerinamerkulova@users.noreply.github.com>
2021-06-23 16:36:51 +0300

    working version with VPN

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 import json
-import requests
+from linkedin_v2 import linkedin
+
 
 cred = json.load(open('credentials.json'))
 
-print(requests.get(
-    'https://api.linkedin.com/v2/jobs',
-    # params = {
-    #     # Any API params go here
-    # },
-    headers={
-        'Authorization': 'Bearer ' + cred['access_token'],
-    },
-).json()
-      )
+def main():
+    application = linkedin.LinkedInApplication(token=cred['access_token'])
+    profile = application.get_profile()
+    print(profile)
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except Exception as e:
+        print(f"[-] Error: {e}")
